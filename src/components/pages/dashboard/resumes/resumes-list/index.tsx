a4ee7682756919891c334d35983f0d5b1ff98458
nielassis <nielvitorba@gmail.com>
2024-12-27 13:12:59 -0300

    feat: use "NewResumeDialog", "AddResumeButton" and "ResumeCard" into list

---
+++ src/components/pages/dashboard/resumes/resumes-list/index.tsx
@@ -0,0 +1,17 @@
+import { AddResumeButton } from "../add-resume-button";
+import { ResumeCard } from "../resumes-card";
+import { NewResumeDialog } from "./new-resume-dialog";
+
+export const ResumesList = () => {
+  return (
+    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 auto-rows-max gap-4 lg:gap-5 flex-1">
+      <NewResumeDialog>
+        <AddResumeButton />
+      </NewResumeDialog>
+
+      <ResumeCard />
+      <ResumeCard />
+      <ResumeCard />
+    </section>
+  );
+};
