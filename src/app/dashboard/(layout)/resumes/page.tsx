948d706fbf16cc9afecf9b8e9c3a04236c12925b
nielassis <nielvitorba@gmail.com>
2024-12-28 19:07:05 -0300

    feat: add next layout group

--- src/app/dashboard/(layout)/resumes/page.tsx
+++ src/app/dashboard/(layout)/resumes/page.tsx
@@ -1,10 +1,10 @@
-import { ResumesList } from "@/components/pages/dashboard/resumes/resumes-list";
-import React from "react";
+import { ResumesList } from "@/components/pages/resumes/resumes-list";
 
 export default function DashboardResumesPage() {
   return (
     <>
       <h1 className="text-4xl font-title font-bold mb-6">Currículos</h1>
+
       <ResumesList />
     </>
   );
