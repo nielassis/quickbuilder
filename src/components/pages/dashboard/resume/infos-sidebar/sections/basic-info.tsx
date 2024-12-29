e4b44b0342bd345f90b17072fb90e32e708bf2ef
nielassis <nielvitorba@gmail.com>
2024-12-29 16:58:10 -0300

    feat: add basic info section w/ input forms

---
+++ src/components/pages/dashboard/resume/infos-sidebar/sections/basic-info.tsx
@@ -0,0 +1,31 @@
+import { UserRound } from "lucide-react";
+import { SectionTitle } from "../section-title";
+import { InputField } from "@/components/ui/input/field";
+import { SwitchField } from "@/components/ui/switch/field";
+
+export const BasicInfoSection = () => {
+  return (
+    <div className="">
+      <SectionTitle title="Informações Básicas" icon={UserRound} />
+
+      <div className="grid grid-cols-2 gap-4 mt-4 w-full">
+        <div className="col-span-full w-full flex gap-3 items-end">
+          <InputField
+            label="Foto"
+            placeholder="..."
+            name="content.image.url"
+            containerClassName="flex-1"
+          />
+
+          <SwitchField name="content.image.visible" className="mb-2" />
+        </div>
+        <InputField label="Nome completo" name="content.infos.fullName" />
+        <InputField label="Cabeçalho" name="content.infos.headline" />
+        <InputField label="E-mail" name="content.infos.email" />
+        <InputField label="Site" name="content.infos.website" />
+        <InputField label="Telefone" name="content.infos.phone" />
+        <InputField label="Localização" name="content.infos.location" />
+      </div>
+    </div>
+  );
+};
