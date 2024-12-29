c0d6ed2d218456a5644b7944959432b93793a8e8
nielassis <nielvitorba@gmail.com>
2024-12-29 16:57:14 -0300

    feat: add components for aside on resume[id] page

--- src/components/pages/dashboard/resume/infos-sidebar/index.tsx
+++ src/components/pages/dashboard/resume/infos-sidebar/index.tsx
@@ -1,3 +1,27 @@
+import Link from "next/link";
+import Logo from "@/assets/logo.svg";
+import { AIgenerationDropdown } from "./ai-generation-dropdown";
+import { Separator } from "@/components/ui/separator";
+import { BasicInfoSection } from "./sections/basic-info";
+import { SummarySection } from "./sections/summary";
+
 export const InfoSideBar = () => {
-  return <aside className="w-full h-full p-6 overflow-y-auto"></aside>;
+  return (
+    <aside className="w-full h-full p-6 overflow-y-auto">
+      <div className="w-full flex items-center justify-between">
+        <Link href="/dashboard/resumes">
+          <Logo className="w-full max-w-[80px]" />
+        </Link>
+
+        <AIgenerationDropdown />
+      </div>
+      <Separator className="my-5" />
+
+      <BasicInfoSection />
+
+      <Separator className="my-5" />
+
+      <SummarySection />
+    </aside>
+  );
 };
